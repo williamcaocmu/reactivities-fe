6bf0129166f920fb201adefe3c88dfdd0e2293ff
William Cao <williamcao.cmu@gmail.com>
2025-04-06 23:37:53 +0700

    03: setup activity details page

--- src/features/activities/details/ActivityDetailPage.tsx
+++ src/features/activities/details/ActivityDetailPage.tsx
@@ -1,29 +1,38 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
+import ActivityDetailHeader from "./ActivityDetaislHeader";
+import ActivityDetailChat from "./ActivityDetailsChat";
+import ActivityDetailInfo from "./ActivityDetailsInfo";
+import ActivityDetailSidebar from "./ActivityDetailsSidebar";
 
-type Props = {};
+export default function ActivityDetailPage() {
+  const activity: Activity | undefined = {
+    id: "1",
+    title: "Activity 1",
+    date: new Date("2021-01-01"),
+    description: "Description 1",
+    category: "Category 1",
+    city: "City 1",
+    venue: "Venue 1",
+    isCanceled: false,
+    latitude: 1,
+    longitude: 1,
+  };
+  const isLoadingActivity = false;
 
-export default function ActivityDetailPage({}: Props) {
+  if (isLoadingActivity) return <Typography>Loading...</Typography>;
+
+  if (!activity) return <Typography>Activity not found</Typography>;
+
   return (
-    <Card sx={{ borderRadius: 3 }}>
-      <CardMedia component="img" src={`/images/categoryImages/food.jpg`} />
-      <CardContent>
-        <Typography variant="h5">Activity title</Typography>
-        <Typography variant="subtitle1" fontWeight="light">
-          {new Date().toLocaleString()}
-        </Typography>
-        <Typography variant="body1">Activity description</Typography>
-      </CardContent>
-      <CardActions>
-        <Button color="primary">Edit</Button>
-        <Button color="inherit">Cancel</Button>
-      </CardActions>
-    </Card>
+    <Grid2 container spacing={3}>
+      <Grid2 size={8}>
+        <ActivityDetailHeader activity={activity} />
+        <ActivityDetailInfo activity={activity} />
+        <ActivityDetailChat />
+      </Grid2>
+      <Grid2 size={4}>
+        <ActivityDetailSidebar />
+      </Grid2>
+    </Grid2>
   );
 }
