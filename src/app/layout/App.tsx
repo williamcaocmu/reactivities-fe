fa93b62b6dcdda8c3f4e36931263c2e4e143f064
William Cao <williamcao.cmu@gmail.com>
2025-04-06 23:46:17 +0700

    04: setup homepage

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,15 +1,25 @@
-import { Container, CssBaseline, Box } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container maxWidth="xl" sx={{ mt: 3 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   );
 }
--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,6 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 const activities = [
   {
@@ -134,8 +135,8 @@ export default function ActivityDashboard() {
       <Grid2 size={7}>
         <ActivityList activities={activities} />
       </Grid2>
-      <Grid2 size={5} sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
-        Activity filters go here
+      <Grid2 size={4}>
+        <ActivityFilters />
       </Grid2>
     </Grid2>
   );
